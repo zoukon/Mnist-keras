20985bc439678434181919db8d2ad0f1cf1e1576
zoukon <richard_isdahl@hotmail.com>
2019-05-10 18:07:53 +0200

    Added comments

--- Keras.py
+++ Keras.py
@@ -10,7 +10,6 @@ from keras.layers import Dense, Dropout, MaxPooling2D, Conv2D, Flatten
 def run_model(epochs = 25, batch_size = 128):
     #load mnist dataset
     #(X_train, y_train), (X_test, y_test) = mnist.load_data() 
-    
     a = np.load("mnist.npz")
     X_test = a['x_test']
     y_test = a['y_test']
@@ -18,15 +17,9 @@ def run_model(epochs = 25, batch_size = 128):
     X_train = a['x_train']
     y_train = a['y_train']
 
-
-
-
     X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
     X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
     input_shape = (28, 28, 1)
-    
-    
-    
     
     #more reshaping
     X_train = X_train.astype('float32')
@@ -42,25 +35,18 @@ def run_model(epochs = 25, batch_size = 128):
     y_train = keras.utils.to_categorical(y_train, num_category)
     y_test = keras.utils.to_categorical(y_test, num_category)
     
-    
-    
-    
-    
     ##model building
     model = Sequential()
     #convolutional layer with rectified linear unit activation
     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    
     #32 convolution filters used each of size 3x3
     #choose the best features via pooling
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    
     #again
     model.add(Conv2D(64, (3, 3), activation='relu'))
     #64 convolution filters used each of size 3x3
     #choose the best features via pooling
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    
     #randomly turn neurons on and off to improve convergence
     model.add(Dropout(0.25))
     #flatten since too many dimensions, we only want a classification output
@@ -71,9 +57,6 @@ def run_model(epochs = 25, batch_size = 128):
     model.add(Dropout(0.5))
     #output a softmax to squash the matrix into output probabilities
     model.add(Dense(num_category, activation='softmax'))
-    
-    #tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
-    
     #We use adam as our optimizer
     #categorical ce since we have multiple classes (10) 
     model.compile(loss=keras.losses.categorical_crossentropy,
@@ -85,11 +68,12 @@ def run_model(epochs = 25, batch_size = 128):
     model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
               verbose=1, validation_data=(X_test, y_test))
     
+    #Print scores
     score = model.evaluate(X_test, y_test, verbose=0)
     print('Test loss:', score[0]) 
     print('Test accuracy:', score[1]) 
     
-    
+    #Save the classifications to a .csv file called results.csv
     Y_predicted = model.predict(X_test)
     pred_label = Y_predicted.argmax(axis = 1)
     image_id = range(1,len(Y_predicted)+1)
@@ -97,8 +81,10 @@ def run_model(epochs = 25, batch_size = 128):
     df = pd.DataFrame(df)
     df.to_csv('results.csv',index = False)
     
-    model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+    # Save the model as a HDF5 file called model.h5
+    model.save('my_model.h5')  
 
+#Allows us to run with 1 or 2 arguments. 1st is number of epochs, second is batch size. 
 if __name__ == '__main__':
     if(len(sys.argv) == 3):
         run_model(int(sys.argv[1]), int(sys.argv[2]) )
@@ -107,6 +93,4 @@ if __name__ == '__main__':
     else:
         run_model()
 
-
-#https://towardsdatascience.com/a-simple-2d-cnn-for-mnist-digit-recognition-a998dbc1e79a
         
